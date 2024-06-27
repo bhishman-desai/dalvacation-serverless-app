3b7c3604e611c017c3fc57f3ff6e68af78b668a5
Bhishman <bh352449@dal.ca>
2024-06-27 17:08:41 -0300

    feat: adding null resource to run the trigger

--- Infrastructure/service.tf
+++ Infrastructure/service.tf
@@ -1,87 +1,4 @@
-/* Define the Cloud Run service using google_cloud_run_v2_service */
-resource "google_cloud_run_v2_service" "dal_vacation_service" {
-  name = "dal-vacation-service"
-
-  location = var.region
-
-  project = var.project_id
-
-  /* Allowing direct access to the service from the internet */
-  ingress = "INGRESS_TRAFFIC_ALL"
-
-  template {
-    scaling {
-      /* Scaling config with min 0 and max 100 CPUs */
-      min_instance_count = 0
-      max_instance_count = 100
-    }
-
-    containers {
-      /* Set the container image from Google Container Registry */
-      image = "gcr.io/${var.project_id}/dal-vacation-service:latest"
-
-      /* Environment Variables */
-      env {
-        name  = "FOO"
-        value = "bar"
-      }
-
-      resources {
-        /* Set the CPU limit to 1 vCPU */
-        limits = {
-          cpu    = "1"
-          memory = "512Mi"
-        }
-      }
-    }
-  }
-
-  traffic {
-    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
-    percent = 100
-  }
-
-  /* Depends on cloud build to fetch and build image from github repo */
-  depends_on = [google_cloudbuild_trigger.github_trigger]
-}
-
-/* Define a Cloud Build trigger for continuous deployment from GitHub */
-resource "google_cloudbuild_trigger" "github_trigger" {
-  name = "github-trigger"
-
-  location = var.region
-
-  /* Service Account */
-  service_account = google_service_account.default.name
-
-  /* GitHub Config */
-  github {
-    owner = "bhishman-desai"
-    name  = "5410-Serverless"
-    push {
-      branch = "^main$"
-    }
-  }
-
-  /* Build steps */
-  build {
-    step {
-      /* Use Docker to build the image */
-      name = "gcr.io/cloud-builders/docker"
-      args = ["build", "-t", "gcr.io/${var.project_id}/dal-vacation-service:latest", "."]
-      dir  = "Frontend/dal-vacation-app" /* Specify the directory of the Dockerfile */
-    }
-
-    /* Specify the image to be pushed to Google Container Registry */
-    images = ["gcr.io/${var.project_id}/dal-vacation-service:latest"]
-
-
-    options {
-      logging = "CLOUD_LOGGING_ONLY"
-    }
-  }
-}
-
+/* Define Service Account */
 resource "google_service_account" "default" {
   account_id = var.account_id
 
@@ -102,3 +19,103 @@ resource "google_project_iam_binding" "logs_writer_binding" {
     "serviceAccount:${google_service_account.default.email}"
   ]
 }
+
+/* Define the Cloud Build trigger for continuous deployment from GitHub */
+resource "google_cloudbuild_trigger" "github_trigger" {
+  name = "github-trigger"
+
+  location = var.region
+
+  /* Service Account */
+  service_account = google_service_account.default.name
+
+  /* GitHub Config TODO: Change this to dalvacation-serverless-app */
+  github {
+    owner = "bhishman-desai"
+    name  = "5410-Serverless"
+    push {
+      branch = "^main$"
+    }
+  }
+
+  /* Build steps */
+  build {
+    step {
+      /* Use Docker to build the image */
+      name = "gcr.io/cloud-builders/docker"
+      args = ["build", "-t", "gcr.io/${var.project_id}/dal-vacation-service:latest", "."]
+      dir  = "Frontend/dal-vacation-app" /* Specify the directory of the Dockerfile */
+    }
+
+    /* Specify the image to be pushed to Google Container Registry */
+    images = ["gcr.io/${var.project_id}/dal-vacation-service:latest"]
+
+    options {
+      logging = "CLOUD_LOGGING_ONLY"
+    }
+  }
+}
+
+/* Define a null resource to run the Cloud Build trigger */
+resource "null_resource" "trigger_run" {
+  triggers = {
+    build_id = google_cloudbuild_trigger.github_trigger.id
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      gcloud builds submit --config cloudbuild.yaml .
+    EOT
+  }
+}
+
+/* Ensure Cloud Run is created after the build trigger completes */
+resource "google_cloud_run_v2_service" "dal_vacation_service" {
+  name = "dal-vacation-service"
+
+  /* Depends on cloud build to fetch and build image from github repo */
+  depends_on = [google_cloudbuild_trigger.github_trigger, null_resource.trigger_run]
+
+  location = var.region
+
+  project = var.project_id
+
+  /* Allowing direct access to the service from the internet */
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    scaling {
+      /* Scaling config with min 0 and max 100 CPUs */
+      min_instance_count = 0
+      max_instance_count = 100
+    }
+
+    containers {
+      /* Set the container image from Google Container Registry */
+      image = "gcr.io/${var.project_id}/dal-vacation-service:latest"
+
+      /* Environment Variables TODO: Change according to need later */
+      env {
+        name  = "FOO"
+        value = "bar"
+      }
+
+      resources {
+        /* Set the CPU limit to 1 vCPU */
+        limits = {
+          cpu    = "1"
+          memory = "512Mi"
+        }
+      }
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+  }
+}
+
+
+
+
