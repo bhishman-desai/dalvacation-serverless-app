666e15f3003a7eedbf96d1309b928e7ce0a40e1f
Bhishman <bh352449@dal.ca>
2024-07-03 17:11:21 -0300

    feat: updating the cloud function

--- MessagePassing/function_code/index.js
+++ MessagePassing/function_code/index.js
@@ -1,14 +1,7 @@
 const functions = require('@google-cloud/functions-framework');
-const { Firestore } = require('@google-cloud/firestore');
-
-const firestore = new Firestore();
 
 functions.cloudEvent('forwardComplaint', async (cloudEvent) => {
     const message = Buffer.from(cloudEvent.data.message.data, 'base64').toString();
-    const collectionRef = firestore.collection('complaint_logs');
-
-    /* Log the message in Firestore */
-    await collectionRef.add({ message, timestamp: Firestore.FieldValue.serverTimestamp() });
 
     /* Forward to a random property agent */
     const propertyAgents = process.env.PROPERTY_AGENTS.split(',');
